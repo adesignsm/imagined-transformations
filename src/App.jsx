c6cefdd626f7b567364a74b333a3198fad60ced8
reyabdul <reyabdul@gmail.com>
2023-02-27 18:54:01 -0500

    set up router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <>
-
-        </>
+        <div className="App">
+            <Routes>
+                <Route path="/" element={ <Home /> } />
+                <Route path="about" element={ <About /> } />
+            </Routes>
+        </div>
     )
 }
 
