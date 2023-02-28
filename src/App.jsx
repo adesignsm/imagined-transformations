072c9411b6f3a336c6ad14c0a10b1336c5250ebc
reyabdul <reyabdul@gmail.com>
2023-02-27 20:19:29 -0500

    set up root.css for global styling (e.g.: variable css, typography, global styling, page style, media queries)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./root.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import About from "./Routes/About"
 
 const App = () => {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={ <Home /> } />
-                <Route path="about" element={ <About /> } />
-            </Routes>
+                <Routes>
+                    <Route path="/" element={ <Home /> } />
+                    <Route path="about" element={ <About /> } />
+                </Routes>
         </div>
     )
 }
