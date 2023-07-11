0d40b6db844a476be08bd40a13a6dd689fff668b
Akash Mulye <akashmulye97@gmail.com>
2023-07-11 15:00:21 -0400

    added logo and changed colours

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Header.css";
 
+import LOGO_PART_1 from "../../Assets/logo/Cycle.svg";
+import LOGO_PART_2 from "../../Assets/logo/Centre.svg";
+
 export const Header = () => {
   return (
-    <section className="header">
+    <header className="header">
       {/*Text will display for header from 480px and larger*/}
       <div className="header__container container grid">
+        <div className="logo-container">
+          <img className="logo-part-1" src={LOGO_PART_1} />
+          <img className="logo-part-2"  src={LOGO_PART_2} />
+        </div>
         <a className="contact-link" href="#contact">
           <p className="contact__link">Send a message to Catherine</p>
         </a>
@@ -15,7 +22,11 @@ export const Header = () => {
         <a href="#contact">
           <i className="uil uil-envelope"></i>
         </a>
+        <div className="logo-container">
+          <img className="logo-part-1" src={LOGO_PART_1} />
+          <img className="logo-part-2"  src={LOGO_PART_2} />
+        </div>
       </div>
-    </section>
+    </header>
   );
 };
