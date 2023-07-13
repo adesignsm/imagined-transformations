569d62d3b03a4dc78e733a7301d7172a890641e8
Akash Mulye <akashmulye97@gmail.com>
2023-07-13 15:46:05 -0400

    final additions

--- src/Routes/Contact/index.jsx
+++ src/Routes/Contact/index.jsx
@@ -3,43 +3,7 @@ import "./Contact.css";
 import { useNav } from "../../Hooks/useNav";
 
 export const Contact = () => {
-  {
-    /* 
-  ----Email handler----
-    ref: https://w3collective.com/react-contact-form/
-*/
-  }
-  const [status, setStatus] = useState("Submit");
-
   const contactRef = useNav("contact");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, phone, subject, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      phone: phone.value,
-      subject: subject.value,
-      message: message.value,
-    };
-    /*
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-
-
-*/
-  };
 
   return (
     <section ref={contactRef} className="contact section" id="contact">
@@ -50,7 +14,15 @@ export const Contact = () => {
           <p className="title__content">
             Get in touch today to book your 30 minute free consultation. Learn
             more about how I can help you get unstuck.
+            <br/>
+            <br />
+            Heads up! If the contact form does not work for you, please click this button to send Catherine a message.
           </p>
+          <button className="secondary-contact__button">
+            <a href="mailto:catherinedavies@imaginedtransformations.com">
+              Send Catherine a Message
+            </a>
+          </button>
         </div>
 
         {/*FORM*/}
@@ -60,35 +32,21 @@ export const Contact = () => {
               <label className="label__title" htmlFor="name">
                 Name:*{" "}
               </label>
-              <input
-                type="text"
-                id="name"
-                placeholder="Enter your name"
-                required
-              />
+              <input type="text" id="name" placeholder="Enter your name" required />
             </div>
 
             <div className="contact__content">
               <label className="label__title" htmlFor="email">
                 Email:*{" "}
               </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="Enter yur email address"
-                required
-              />
+              <input type="email" id="email" placeholder="Enter yur email address" required />
             </div>
 
             <div className="contact__content">
               <label className="label__title" htmlFor="phone">
                 Phone:{" "}
               </label>
-              <input
-                type="text"
-                id="phone"
-                placeholder="Enter your phone number"
-              />
+              <input type="text" id="phone" placeholder="Enter your phone number" />
             </div>
 
             <div className="contact__content">
@@ -102,13 +60,7 @@ export const Contact = () => {
               <label className="label__title" htmlFor="message">
                 Message:{" "}
               </label>
-              <textarea
-                name="message"
-                id="message"
-                rows="3"
-                placeholder="Type your message to Catherine here."
-                required
-              ></textarea>
+              <textarea name="message" id="message" rows="3" placeholder="Type your message to Catherine here." required></textarea>
             </div>
             {/*Made submit button but not sure where to send afterwards */}
             <button className="form__button" type="submit">Submit</button>
